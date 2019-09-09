e3f738659eeb0fb5116c48b5b146168306369b67
raimsten <higonet.r@gmail.com>
2019-09-08 21:42:39 -0300

    init

---
+++ service/api/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('employees/', views.employees, name='employees'), # list of employees
+    path('employees/<int:employee_id>/', views.employees, name='employees'), # detail of employee
+]
